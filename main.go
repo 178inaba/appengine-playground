72fcddd51a7e31e8bd49d157fd4d51217ac60d4f
Masahiro Furudate <178inaba.git@gmail.com>
2020-07-11 23:54:21 +0900

    Add to set log level info

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := echo.New()
-
+	e.Logger.SetLevel(log.INFO)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
