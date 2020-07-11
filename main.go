0e51515419327de330565455b24624d8f2e1fd22
Masahiro Furudate <178inaba.git@gmail.com>
2020-07-12 01:51:27 +0900

    Add Entry

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"cloud.google.com/go/logging"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/labstack/gommon/log"
+	echolog "github.com/labstack/gommon/log"
 )
 
 func main() {
@@ -29,7 +32,7 @@ func main() {
 	// ---
 
 	e := echo.New()
-	e.Logger.SetLevel(log.INFO)
+	e.Logger.SetLevel(echolog.INFO)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -48,5 +51,46 @@ func hello(c echo.Context) error {
 	c.Logger().Info("Start.")
 	defer c.Logger().Info("End.")
 
+	var trace string
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID != "" {
+		traceHeader := c.Request().Header.Get("X-Cloud-Trace-Context")
+		traceParts := strings.Split(traceHeader, "/")
+		if len(traceParts) > 0 && len(traceParts[0]) > 0 {
+			trace = fmt.Sprintf("projects/%s/traces/%s", projectID, traceParts[0])
+		}
+	}
+
+	log.Println(Entry{
+		Severity:  "NOTICE",
+		Message:   "This is the default display field.",
+		Component: "arbitrary-property",
+		Trace:     trace,
+	})
+
+	fmt.Fprintln(c.Response().Writer, "Hello Logger!")
+
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+// Entry defines a log entry.
+type Entry struct {
+	Message  string `json:"message"`
+	Severity string `json:"severity,omitempty"`
+	Trace    string `json:"logging.googleapis.com/trace,omitempty"`
+
+	// Stackdriver Log Viewer allows filtering and display of this as `jsonPayload.component`.
+	Component string `json:"component,omitempty"`
+}
+
+// String renders an entry structure to the JSON format expected by Stackdriver.
+func (e Entry) String() string {
+	if e.Severity == "" {
+		e.Severity = "INFO"
+	}
+	out, err := json.Marshal(e)
+	if err != nil {
+		log.Printf("json.Marshal: %v", err)
+	}
+	return string(out)
 }
