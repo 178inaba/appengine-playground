8fb287e318d28834bb6bd1587e0e73fc7790f6e7
Masahiro Furudate <178inaba.git@gmail.com>
2020-07-12 21:58:29 +0900

    Fix logger middleware

--- log/middleware.go
+++ log/middleware.go
@@ -2,39 +2,56 @@ package log
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/logging"
 	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
 	"github.com/labstack/echo/v4"
+	mrpb "google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
-type IndexHandler struct {
-	requestLogger     *logging.Logger
-	applicationLogger *logging.Logger
+type LoggerMiddleware struct {
+	client *logging.Client
+
+	moduleID  string
+	projectID string
+	versionID string
+	zone      string
 }
 
-func (h *IndexHandler) Logger(next echo.HandlerFunc) echo.HandlerFunc {
+func (m *LoggerMiddleware) Logger(next echo.HandlerFunc) echo.HandlerFunc {
+	hf := &propagation.HTTPFormat{}
+
+	opt := logging.CommonResource(&mrpb.MonitoredResource{
+		Type: "gae_app",
+		Labels: map[string]string{
+			"module_id":  m.moduleID,
+			"project_id": m.projectID,
+			"version_id": m.versionID,
+			"zone":       m.zone,
+		},
+	})
+	reqLogger := m.client.Logger(fmt.Sprintf("%s_request", m.moduleID), opt)
+	appLogger := m.client.Logger(fmt.Sprintf("%s_application", m.moduleID), opt)
 	return func(c echo.Context) error {
+		req := c.Request()
+		sc, _ := hf.SpanContextFromRequest(req)
+		trace := fmt.Sprintf("projects/%s/traces/%s", m.projectID, sc.TraceID)
+		spanID := sc.SpanID.String()
+
+		logger := New(appLogger, trace, spanID)
+		c.Echo().Logger = logger // TODO
+
 		start := time.Now()
 		if err := next(c); err != nil {
-			c.Error(err)
+			c.Error(err) // TODO
 		}
-
 		end := time.Now()
 
-		projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-		req := c.Request()
-		remoteIP := strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]
-
 		resp := c.Response()
-
-		hf := &propagation.HTTPFormat{}
-		sc, _ := hf.SpanContextFromRequest(req)
-		trace := fmt.Sprintf("projects/%s/traces/%s", projectID, sc.TraceID)
-		h.requestLogger.Log(logging.Entry{
+		remoteIP := strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]
+		reqLogger.Log(logging.Entry{
 			Timestamp: time.Now(),
 			Severity:  logging.Error,
 			HTTPRequest: &logging.HTTPRequest{
@@ -46,7 +63,7 @@ func (h *IndexHandler) Logger(next echo.HandlerFunc) echo.HandlerFunc {
 			},
 			Trace:        trace,
 			TraceSampled: true,
-			SpanID:       sc.SpanID.String(),
+			SpanID:       spanID,
 		})
 
 		return nil
