aa043f94a16ca0ab0d58730a71d9d6b2530c6cbd
Masahiro Furudate <178inaba.git@gmail.com>
2020-07-12 22:11:48 +0900

    Using LoggerMiddleware

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ import (
 	"time"
 
 	"cloud.google.com/go/logging"
-	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
+	logginglog "github.com/178inaba/appengine-playground/log"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echolog "github.com/labstack/gommon/log"
-	mrpb "google.golang.org/genproto/googleapis/api/monitoredres"
-	logpb "google.golang.org/genproto/googleapis/logging/v2"
 )
 
 func main() {
@@ -37,26 +35,16 @@ func main() {
 	defer loggingClient.Close()
 
 	service := os.Getenv("GAE_SERVICE")
-	opt := logging.CommonResource(&mrpb.MonitoredResource{
-		Type: "gae_app",
-		Labels: map[string]string{
-			"module_id":  service,
-			"project_id": projectID,
-			"version_id": os.Getenv("GAE_VERSION"),
-			"zone":       zone,
-		},
-	})
-	reqLogger := loggingClient.Logger(fmt.Sprintf("%s_request", service), opt)
-	appLogger := loggingClient.Logger(fmt.Sprintf("%s_application", service), opt)
-
-	ih := &IndexHandler{requestLogger: reqLogger, applicationLogger: appLogger}
+	version := os.Getenv("GAE_VERSION")
+	lm := logginglog.NewLoggerMiddleware(loggingClient, service, projectID, version, zone)
 
 	e := echo.New()
 	e.Logger.SetLevel(echolog.INFO)
-	e.Use(ih.Logger)
+	e.Use(lm.Logger)
 	e.Use(middleware.Recover())
 
-	e.GET("/", ih.index)
+	e.GET("/", index)
+	e.GET("/sleep", sleep)
 	e.GET("/hello", hello)
 	e.GET("/_ah/warmup", func(c echo.Context) error { return c.NoContent(http.StatusNoContent) })
 
@@ -69,73 +57,20 @@ func main() {
 	e.Logger.Fatalf("%v.", e.Start(fmt.Sprintf(":%s", port)))
 }
 
-type IndexHandler struct {
-	requestLogger     *logging.Logger
-	applicationLogger *logging.Logger
-}
-
-func (h *IndexHandler) Logger(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		start := time.Now()
-		if err := next(c); err != nil {
-			c.Error(err)
-		}
-
-		end := time.Now()
-
-		projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-		req := c.Request()
-		remoteIP := strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]
-
-		resp := c.Response()
-
-		hf := &propagation.HTTPFormat{}
-		sc, _ := hf.SpanContextFromRequest(req)
-		trace := fmt.Sprintf("projects/%s/traces/%s", projectID, sc.TraceID)
-		h.requestLogger.Log(logging.Entry{
-			Timestamp: time.Now(),
-			Severity:  logging.Error,
-			HTTPRequest: &logging.HTTPRequest{
-				Request:      req,
-				Latency:      end.Sub(start),
-				Status:       resp.Status,
-				RemoteIP:     remoteIP,
-				ResponseSize: resp.Size,
-			},
-			Trace:        trace,
-			TraceSampled: true,
-			SpanID:       sc.SpanID.String(),
-		})
-
-		return nil
-	}
-}
-
-func (h *IndexHandler) index(c echo.Context) error {
-	c.Logger().Info("Start.")
-	defer c.Logger().Info("End.")
-
-	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-
-	req := c.Request()
-	hf := &propagation.HTTPFormat{}
-	sc, _ := hf.SpanContextFromRequest(req)
-	trace := fmt.Sprintf("projects/%s/traces/%s", projectID, sc.TraceID)
-	h.applicationLogger.Log(logging.Entry{
-		Timestamp:    time.Now(),
-		Severity:     logging.Critical,
-		Trace:        trace,
-		TraceSampled: true,
-		SourceLocation: &logpb.LogEntrySourceLocation{
-			File:     "main.go",
-			Line:     101,
-			Function: "index",
-		},
-		SpanID:  sc.SpanID.String(),
-		Payload: "hello log!!",
-	})
+func index(c echo.Context) error {
+	c.Logger().Info("Start index.")
+	defer c.Logger().Info("End index.")
 
 	return c.String(http.StatusTeapot, "Index!")
+}
+
+func sleep(c echo.Context) error {
+	c.Logger().Info("Start sleep.")
+	defer c.Logger().Info("End sleep.")
+
+	time.Sleep(10 * time.Second)
+
+	return c.String(http.StatusNoContent, "Sleep!")
 }
 
 func hello(c echo.Context) error {
